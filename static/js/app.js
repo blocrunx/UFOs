c9e001f69af5e3f68ef80bb008a26fa35d8ce2d8
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-05-08 14:01:41 -0400

    filter fields added and working

--- static/js/app.js
+++ static/js/app.js
@@ -23,32 +23,75 @@ function buildTable(data) {
       );
     });
   };
+var filterObject = {};
 
-  function handleClick() {
-    // Grab the datetime value from the filter
-    let place = d3.select("#city").property("value");
-    let date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
+  function buildFilters() {
+    // Grab the entered values ot be filtered
+    let sightingDate = d3.select("#datetime").property("value");
+    let sightingPlace = d3.select("#city").property("value");
+    let sightingState = d3.select("#state").property("value");
+    let sightingCountry = d3.select("#country").property("value");
+    let sightingShape = d3.select("#shape").property("value");
+    
+    
     
      // Check to see if a date was entered and filter the
     // data using that date.
-    if (place) {
+    /*if (place) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value*/
+      //filteredData = filteredData.filter(row => row.city === place);
+
+    //};
+    if (sightingDate) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.city === place);
+      filterObject.date=date;
     };
-    if (date) {
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
+    if(sightingPlace) {
+      filterObject.city=sightingPlace;
     };
+
+    if (sightingState){
+      filterObject.state=sightingState;
+    };
+
+    if (sightingCountry){
+      filterObject.country=sightingCountry;
+    };
+
+    if (sightingShape){
+      filterObject.shape=sightingShape;
+    };
+    
+  
     
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
-    buildTable(filteredData);
+    filterTable();
   };
+
+  function filterTable() {
+
+    // Set the filteredData to the tableData
+    let filteredData = tableData
+    // Loop through all of the filters and keep any data that
+    // matches the filter values
+    
+    // First convert the object into an array of entries
+    const entries = Object.entries(filterObject)
+
+    // Destructure the array into its key and property
+    for (const [key, value] of entries) {
+      filteredData = filteredData.filter(row => row[key] === value);
+    }
+      
+
+    // Finally, rebuild the table using the filtered Data
+    buildTable(filteredData);
+  }
   
-  d3.select("#filter-btn").on("click", handleClick);
+  d3.selectAll("#filter-btn").on("click", buildFilters);
 
   buildTable(tableData);
