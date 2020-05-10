0f0589adaa0a625106721a3fcbb9aa8829b04b37
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-05-10 12:39:24 -0400

    submission ready

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// import the data from data.js
+// Import the data from data.js
 const tableData = data;
 
 // Reference the thml using tbody
@@ -8,14 +8,14 @@ function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
   
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
+    /* Next, loop through each object in the data
+    and append a row and cells for each value in the row */
     data.forEach((dataRow) => {
       // Append a row to the table body
       let row = tbody.append("tr");
   
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
+      /* Loop through each field in the dataRow and add
+      each value as a table cell (td) */
       Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
         cell.text(val);
@@ -31,8 +31,7 @@ var filterObject = {};
     let sightingPlace = d3.select("#city").property("value");
     let sightingState = d3.select("#state").property("value");
     let sightingCountry = d3.select("#country").property("value");
-    let sightingShape = d3.select("#shape").property("value");
-    
+    let sightingShape = d3.select("#shape").property("value");    
         
     // Conditionals to check user entered filter entries 
     if (sightingDate){
@@ -53,35 +52,32 @@ var filterObject = {};
 
     if (sightingShape){
       filterObject.shape=sightingShape;
-    };
-    
+    };    
        
-     // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
+     /*  Rebuild the table using the filtered data
+     @NOTE: If no date was entered, then filteredData will
+     just be the original tableData. */
     filterTable();
   };
 
   function filterTable() {
 
     // Set the filteredData to the tableData
-    let filteredData = tableData
-    // Loop through all of the filters and keep any data that
-    // matches the filter values
+    let filteredData = tableData;
+    /* Loop through all of the filters and keep any data that
+    matches the filter values */
     
     // First convert the object into an array of entries
-    const entries = Object.entries(filterObject)
+    const entries = Object.entries(filterObject);
 
     // Destructure the array into its key and property
     for (const [key, value] of entries) {
       filteredData = filteredData.filter(row => row[key] === value);
-    }
-      
+    };      
 
     // Finally, rebuild the table using the filtered Data
     buildTable(filteredData);
-  }
+  }; 
   
   d3.selectAll("#filter-btn").on("click", buildFilters);
-
   buildTable(tableData);
